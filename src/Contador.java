b6370429e98817ea3febbd8587cfe68ebde8b442
usarnamevictor <joaovictorly90@gmail.com>
2025-03-13 14:24:06 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println("Erro:" + exception.getMessage());
+		}
+		terminal.close();
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O primeiro parâmetro não pode ser maior que o segundo.");
+        }
+		int contagem = parametroDois - parametroUm;
+		for ( int valor = 0; valor < contagem; valor++) {
+            System.out.println("Imprimindo o número:" + valor);
+        }
+	}
+}
